73afc6a6e3a7d54ef512d3e20554718157231a1b
Pedro Silva <128435090+Silvape18@users.noreply.github.com>
2023-06-09 21:21:48 -0300

    Create Atividade2.java

---
+++ Atividade2.java
@@ -0,0 +1,53 @@
+# AToracle.java
+
+public class Atividade2{
+     public static void main(String[] args) throws Exception{
+            System.out.println("============Problema1========");
+            System.out.println("                             ");
+            System.out.println("        #  #  #  #           ");
+            System.out.println("       #          #          ");
+            System.out.println("     #    #    #    #        ");
+            System.out.println("     #    #    #    #        ");
+            System.out.println("     #      ##      #        ");
+            System.out.println("     #              #        ");
+            System.out.println("      #            #         ");
+            System.out.println("        #  #  #  #           ");
+            System.out.println("                              ");
+            System.out.println("==========Problema 2==========");
+            System.out.println("                              ");
+            System.out.println("        XXXXXXXXXXXXXXXXX     ");
+            System.out.println("       X                 X    ");
+            System.out.println("       X    O      O     X    ");
+            System.out.println("       X                 X    ");
+            System.out.println("       X    *______*     X    ");
+            System.out.println("       X                 X    ");
+            System.out.println("       X       ===       X    ");
+            System.out.println("        XXXXXXXXXXXXXXXXX     ");
+            System.out.println("                              ");
+            
+            //PROBLEMA 3, VOU RESPONDER AQUI COMO COMENTARIO:
+            //AS CLASSES QUE EU ENCONTREI SÃO:
+            //CLIENTE, CAIXA, TRANSPORTE 
+
+
+
+
+
+
+          
+
+
+
+
+
+
+
+
+         }
+
+         
+
+
+
+         
+    }
