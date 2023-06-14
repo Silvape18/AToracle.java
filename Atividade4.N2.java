3acac7f38a6ba0a916e866a7c91a0457d9ea013b
Pedro Silva <128435090+Silvape18@users.noreply.github.com>
2023-06-14 16:37:11 -0300

    Atividade4.N2.java

---
+++ Atividade4.N2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Atividade4{
+     public static void main(String[] args) throws Exception{
+
+         Scanner teclado = new Scanner(System.in);
+
+
+         System.out.println("Digite o valor do primeiro lado: ");
+                         double lado1 = teclado.nextDouble();
+            System.out.print("Digite o valor do segundo lado: ");
+                double lado2 = teclado.nextDouble();
+
+                double hipotenusa = calcularHipotenusa(lado1, lado2);
+
+                 System.out.println("A hipotenusa do triângulo é: " + hipotenusa);
+
+    }
+
+        public static double calcularHipotenusa(double lado1, double lado2){
+        double hipotenusa = Math.sqrt(Math.pow(lado1, 2) + Math.pow(lado2, 2));
+        return hipotenusa;
+    }
+}       
+       
+       
+            
+
